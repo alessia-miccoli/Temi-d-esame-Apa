3cea226bb0abd2de8ee4b808951424d7e86014a5
coccocarmiano <59957277+coccocarmiano@users.noreply.github.com>
2020-01-23 17:53:56 +0100

    Add files via upload

--- 22Febbraio2017es2-12pt.c
+++ 22Febbraio2017es2-12pt.c
@@ -1,85 +1,76 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
-typedef struct nodo *link;
+typedef struct node_t {
+	char key;
+	struct node_t* child;
+	struct node_t* sibling;
+	struct node_t* father;
+	int numberOfChilds;
+} node, *link;
 
-struct nodo{
-int nodo_val;
-link l,r,p;
-int nodo_figli;
-int nodo_livello;
-char *nodo_str;
-};
+link newborn(char key, link child, link sibling);
+link tribu();
+void censimento(link root);
+void processTree(link root);
 
-link Crea_Nodo(int val,link sx,link dx,link padre,int figli,int livello,char *str){
-link x=malloc(sizeof(*x));
-x->nodo_val=val;
-x->l=sx;
-x->r=dx;
-x->p=padre;
-x->nodo_figli=figli;
-x->nodo_livello=livello;
-x->nodo_str=strdup(str);
-return x;
+int main(int argc, char **argv)
+{
+	link albero = tribu();
+	censimento(albero);
+	processTree(albero);
+	printf("\nProcessTree()...\n\n");
+	censimento(albero);
 }
 
-link inserimento_nodi_BST(link h,link s,int val,int liv,char *str){
-if(h==s){
-h=Crea_Nodo(val,NULL,NULL,NULL,0,liv,str);
-h->p=h;/*Lo faccio perché la radice del BST ha come padre se stesso,altrimenti (stampa_BST) da errore*/
-return h;
-    }
-if(val<=h->nodo_val){
-h->l=inserimento_nodi_BST(h->l,s,val,liv+1,str);
-}else if(val>h->nodo_val){
-h->r=inserimento_nodi_BST(h->r,s,val,liv+1,str);
-}
-return h;
+link newborn(char key, link child, link sibling){
+	// Questa è la funzione di creazione.
+	// Non include numero di figli / padre perchè
+	// Vanno aggiunti dopo con la funzione dell'esercizio
+	link n = malloc(sizeof(node));
+	n->key = key;
+	n->child = child;
+	n->sibling = sibling;
+	n->father = NULL;
+	n->numberOfChilds=-1; // So che sarebbe children ma ormai
+	return n;
 }
 
-/*INIZIO->FUNZIONE_RICORSIVA*/
+link tribu(){
+	// L'albero in foto
+	link b, h;
+	h = newborn('j', NULL, NULL);
+	h = newborn('i', NULL, h);
+	h = newborn('h', NULL, h);
+	h = newborn('e', h, NULL);
+	b = newborn('g', NULL, NULL);
+	b = newborn('f', NULL, b);
+	b = newborn('d', b, NULL);
+	b = newborn('c', NULL, b);
+	b = newborn('b', h, b);
+	return newborn('a', b, NULL);
+}
+
+void censimento(link root){
+	if (root == NULL) return;
+	printf("Nome: %c Numero di Figli: %c Padre: %c\n",
+			root->key,
+			root->numberOfChilds > -1 ? (char)( (int)'0' + root->numberOfChilds ) : '?',
+			root->father == NULL ? '?' : root->father->key);
+	censimento(root->child);
+	censimento(root->sibling);
+}
+
+void processTreeR(link actual, link father){
+	if (actual == NULL) return;
+	if (actual->numberOfChilds == -1) actual->numberOfChilds = 0;
+	actual->father = father;
+	if (father != NULL) father->numberOfChilds++;
+	processTreeR(actual->sibling, father);
+	processTreeR(actual->child, actual);
+
+}
 
 void processTree(link root){
-link tp;
-if(root==NULL){
-return;
-}
-processTree(root->l);
-if(root->l!=NULL){
-for(tp=root->l;tp!=NULL;tp=tp->r){
-    (root->nodo_figli)++;
-    tp->p=root;
-        }
-    }
-processTree(root->r);
-}
-
-/*FINE->FUNZIONE_RICORSIVA*/
-
-void stampa_BST(link h,link s){
-if(h==s){
-return;
-    }
-printf("nodo_str->%s , figli->%d , nodo_padre->%s\n",h->nodo_str,h->nodo_figli,h->p->nodo_str);
-stampa_BST(h->l,s);
-stampa_BST(h->r,s);
-}
-
-int main()
-{
-link rad=NULL;
-rad=inserimento_nodi_BST(rad,NULL,15,0,"a");
-rad=inserimento_nodi_BST(rad,NULL,9,0,"b");
-rad=inserimento_nodi_BST(rad,NULL,10,0,"c");
-rad=inserimento_nodi_BST(rad,NULL,13,0,"d");
-rad=inserimento_nodi_BST(rad,NULL,11,0,"f");
-rad=inserimento_nodi_BST(rad,NULL,12,0,"g");
-rad=inserimento_nodi_BST(rad,NULL,8,0,"e");
-rad=inserimento_nodi_BST(rad,NULL,5,0,"h");
-rad=inserimento_nodi_BST(rad,NULL,6,0,"i");
-rad=inserimento_nodi_BST(rad,NULL,7,0,"j");
-processTree(rad);
-stampa_BST(rad,NULL);
-return 0;
+	processTreeR(root, NULL);
 }
