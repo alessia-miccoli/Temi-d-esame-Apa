fe787f82fa8ff9e829b8e10a8ea728ad2079b07b
Simone Candido <59175940+SimoneMoreWare@users.noreply.github.com>
2023-02-04 15:09:56 +0100

    Update es3.c

--- 2020/appello3/es3.c
+++ 2020/appello3/es3.c
@@ -1,60 +1,102 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 
-#define MAX_ITEMS 100
-#define MAX_COST 1000
-
-typedef struct {
+typedef struct{
     char name[100];
-    int cost;
-    int value;
-    int type;
-    int quantity;
-} item_t;
+    int costo;
+    int valore;
+    int tipo;
+    int quantita;
+}oggetto;
 
-int n, c;
-item_t items[MAX_ITEMS];
-int types_count;
-int max_value;
+int solve(oggetto oggetti[5],int n,int c);
+void solve_r(int pos,int k,int *sol,oggetto oggetti[5],int n,int c,int costo,int *valoremassimo,int *best);
+int check(int k,oggetto oggetti[5],int *sol);
 
-void read_input() {
-    scanf("%d %d", &n, &c);
-    for (int i = 0; i < n; i++) {
-        scanf("%s %d %d %d %d", items[i].name, &items[i].cost, &items[i].value, &items[i].type, &items[i].quantity);
-        types_count = types_count > items[i].type ? types_count : items[i].type;
-    }
-}
-
-int count_types(int mask) {
-    int count = 0;
-    for (int i = 0; i <= types_count; i++) {
-        if (mask & (1 << i)) {
-            count++;
-        }
-    }
-    return count;
-}
-
-void backtrack(int i, int cost, int value, int mask) {
-    if (i == n) {
-        if (cost <= c && count_types(mask) == n && value > max_value) {
-            max_value = value;
-        }
-        return;
-    }
-    for (int j = 0; j <= items[i].quantity; j++) {
-        int new_cost = cost + j * items[i].cost;
-        int new_value = value + j * items[i].value;
-        int new_mask = mask | (1 << items[i].type);
-        if (new_cost <= c) {
-            backtrack(i + 1, new_cost, new_value, new_mask);
-        }
-    }
-}
-
-int main() {
-    read_input();
-    backtrack(0, 0, 0, 0);
-    printf("Max value: %d\n", max_value);
+int main(){
+    int n=5;
+    oggetto oggetti[5] = {
+            {"uno",3,4,1,2},
+            {"due",2,3,2,3},
+            {"tre",4,5,3,1},
+            {"quattro",1,2,4,2},
+            {"cinque",5,7,5,1}
+    };
+    int c=10;
+    int res;
+    res=solve(oggetti,n,c);
+    printf("%d",res);
     return 0;
 }
+
+int solve(oggetto oggetti[5],int n,int c){
+    int *sol = (int *) calloc(n,sizeof(int));
+    int *best = (int *) calloc(n,sizeof(int));
+    int pos=0;
+    int i;
+    int costo=0,valore;
+    int costotmp;
+    int valoremassimo=0;
+    for(i=1;i<=n;i++) solve_r(pos,i,sol,oggetti,n,c,costo,&valoremassimo,best);
+    for(i=0;i<n;i++){
+        if(best[i]!=0){
+            costotmp=best[i]*oggetti[i].costo;
+            valore=best[i]*oggetti[i].valore;
+            printf("-%s costo:%d valore:%d tipo:%d quantita:%d-",oggetti[i].name,costotmp,valore,oggetti[i].tipo,best[i]);
+        }
+    }
+    printf("\n");
+    free(best);
+    free(sol);
+    return valoremassimo;
+}
+
+void solve_r(int pos,int k,int *sol,oggetto oggetti[5],int n,int c,int costo,int *valoremassimo,int *best){
+
+    int i,j;
+    int new_cost;
+    int costoparziale,valoreparziale;
+    if(pos>=k){
+        costoparziale=0;
+        for(i=0;i<k;i++){
+                if(sol[i]!=0){
+                    costoparziale = costoparziale + sol[i]*oggetti[i].costo;
+                }
+            }
+            if(costoparziale<=c) {
+                valoreparziale=0;
+                for(j=0;j<k;j++){
+                    if(sol[j]!=0){
+                        valoreparziale=valoreparziale+sol[j]*oggetti[j].valore;
+                    }
+                }
+                if((valoreparziale)>(*valoremassimo)){
+                    (*valoremassimo)=valoreparziale;
+                    for(j=0;j<k;j++) best[j]=sol[j];
+                }
+            }
+        return;
+    }
+
+    for(i=0;i<=oggetti[pos].quantita;i++){
+        new_cost=costo+i*oggetti[i].costo;
+        if(new_cost<=c && check(pos,oggetti,sol)){
+                sol[pos]=i;
+                solve_r(pos+1,k,sol,oggetti,n,c,new_cost,valoremassimo,best);
+        }
+        }
+}
+
+int check(int k,oggetto oggetti[5],int *sol){
+    int i,j;
+    for(i=0;i<k;i++){
+        for(j=i+1;j<k;j++){
+            if(sol[i]!=0 && sol[j]!=0){
+                if(oggetti[i].tipo==oggetti[j].tipo) return 0;
+            }
+        }
+    }
+    return 1;
+}
+
+
