659fec64e330ff8e3d07dd07b48d54b0b57e6257
Simone Candido <59175940+SimoneMoreWare@users.noreply.github.com>
2023-02-08 12:40:18 +0100

    Update T.h

--- 2023/appello1/es2/T.h
+++ 2023/appello1/es2/T.h
@@ -3,6 +3,13 @@
 #include <stdlib.h>
 #include <stdio.h>
 typedef struct tree *TREE;
+typedef struct node *link;
+
+struct node{
+    int data;
+    link *children;
+    int N;
+};
 int f(TREE t);
 TREE init();
 
